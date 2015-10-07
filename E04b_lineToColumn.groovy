82a73c5ad7d5175fb9c433384aa77d77d134cf86
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-10-07 18:31:32 +0100

    First commit. Code from first part of Exercise 4 provided as starting point.

---
+++ E04b_lineToColumn.groovy
@@ -0,0 +1,6 @@
+println "Please enter some text. Finish by pressing return.";
+String s = System.console().readLine();
+for (int count = 0; count < s.length(); count++) {
+	char c = s.charAt(count);
+	println c;
+}
