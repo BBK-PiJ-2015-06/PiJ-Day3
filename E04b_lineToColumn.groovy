016a736e6ee05d1e6766db4fa3ab48df655a7a51
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-10-07 18:36:23 +0100

    if statement added to distinguish 'space' from other characters. Solution to second part of exercise 4 completed.

--- E04b_lineToColumn.groovy
+++ E04b_lineToColumn.groovy
@@ -2,5 +2,9 @@ println "Please enter some text. Finish by pressing return.";
 String s = System.console().readLine();
 for (int count = 0; count < s.length(); count++) {
 	char c = s.charAt(count);
-	println c;
+	if (c == ' ') {
+		println " ";
+	} else {
+		print c;
+	}
 }
