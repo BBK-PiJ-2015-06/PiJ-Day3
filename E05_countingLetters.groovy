7bdcbfb049b95087705647cf76c6c3aceff2c1fe
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-10-07 18:50:20 +0100

    Solution to exercise 5 completed.

--- E05_countingLetters.groovy
+++ E05_countingLetters.groovy
@@ -1,2 +1,10 @@
 println "Please enter some text. Finish by pressing 'return'.";
 String s = System.console().readLine();
+int output = 0
+for (int count = 0; count < s.length(); count++) {
+	char c = s.charAt(count);
+	if (c == 'e') {
+		output = output + 1;
+	}
+}
+println output
