b01b70b41b7897e70e0e8b692bbb2cbbc2054a9f
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-10-07 20:30:34 +0100

    Program amended to ask for character from user. Solution to second part of exercise 5 completed.

--- E05_countingLetters.groovy
+++ E05_countingLetters.groovy
@@ -1,10 +1,12 @@
 println "Please enter some text. Finish by pressing 'return'.";
 String s = System.console().readLine();
+print "Please enter a character to be searched in the above text: ";
+String character = System.console().readLine();
 int output = 0
 for (int count = 0; count < s.length(); count++) {
 	char c = s.charAt(count);
-	if (c == 'e') {
+	if (c == character) {
 		output = output + 1;
 	}
 }
-println output
+println "There are " + output + " instances of the character " + "'" + character + "'" + " in the text."
