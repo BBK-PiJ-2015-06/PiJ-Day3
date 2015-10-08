52c4330e3f6117a9c39e4cd4de5988d62fa6cd89
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-10-08 10:37:21 +0100

    Added code to now ask user for point2 and point3 coordinates.

--- E12_distanceP2P.groovy
+++ E12_distanceP2P.groovy
@@ -5,8 +5,25 @@ point1.x = Double.parseDouble(s);
 print "Please input y coordinate for Point 1: ";
 s = System.console().readLine();
 point1.y = Double.parseDouble(s);
-println point1.x + ", " + point1.y
+println "Point 1: " + point1.x + ", " + point1.y
 
+Point point2 = new Point();
+print "Please input x coordinate for Point 2: ";
+s = System.console().readLine();
+point2.x = Double.parseDouble(s);
+print "Please input y coordinate for Point 2: ";
+s = System.console().readLine();
+point2.y = Double.parseDouble(s);
+println "Point 2: " + point2.x + ", " + point2.y
+
+Point point3 = new Point();
+print "Please input x coordinate for Point 3: ";
+s = System.console().readLine();
+point3.x = Double.parseDouble(s);
+print "Please input y coordinate for Point 3: ";
+s = System.console().readLine();
+point3.y = Double.parseDouble(s);
+println "Point 3: " + point3.x + ", " + point3.y
 
 class Point {
 	double x;
