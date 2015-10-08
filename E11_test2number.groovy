ab4385f7209a0c6b2899848d4564bb2c51f76786
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-10-08 09:59:00 +0100

    Solution to exercise 11 solved for positive numbers.

--- E11_test2number.groovy
+++ E11_test2number.groovy
@@ -1,4 +1,4 @@
-print "Please enter a number, including commas and decimal points if necessary: ";
+println "Please enter a number, including commas and decimal points if necessary: ";
 String s = System.console().readLine();
 String num = s;
 boolean negative = false;
@@ -11,7 +11,8 @@ if (s.substring(0,1) == "-") {
 	num = s.substring(0,1);
 	count = 1
 }
-int decimalpos = s.length()
+int decimalpos = s.length();
+int commacount = 0;
 for (count; count < s.length(); count++) {
 	char c = s.charAt(count);
 	if (c != ',') {
@@ -22,14 +23,19 @@ for (count; count < s.length(); count++) {
 		} 
 	}
 }
-int number = Integer.parseInt(num);
-if (number%2 == 1) {
-	number = (((number - 1) / 2) * 10) + 5;	
-} else {
-	number = number / 2;
+int decimalplaces = s.length() - decimalpos
+if (decimalplaces > 0) {
+	decimalplaces = decimalplaces - 1;
 }
-String output = Integer.toString(number) 
-int outputlength = output.length()
+int number = Integer.parseInt(num);
+if (number%2 == 0) {
+	number = number / 2;
+} else {
+	number = (((number - 1)/2) * 10) + 5;
+	decimalplaces ++;
+}
+double output = number;
+for (decimalplaces; decimalplaces > 0; decimalplaces = decimalplaces - 1) {
+	output = output / 10;
+}
 println output
-println outputlength
-println decimalpos
