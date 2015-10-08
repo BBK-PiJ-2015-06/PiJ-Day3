c37654bd44bddda30e23d4dbdf257653f27cd475
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-10-08 11:55:16 +0100

    Code added to lines 1 - 14, asking user for inputs. Creates 2 Point object from input.

--- E13_rectangle.groovy
+++ E13_rectangle.groovy
@@ -1,3 +1,20 @@
+Point point1 = new Point();
+print "Please input x coordinate of Point 1: ";
+String s = System.console().readLine();
+point1.x = Double.parseDouble(s);
+print "Please input y coordinate of Point 1: ";
+s = System.console().readLine();
+point1.y = Double.parseDouble(s);
+Point point2 = new Point();
+print "Please input x coordinate of Point 2: ";
+s = System.console().readLine();
+point2.x = Double.parseDouble(s);
+print "Please input y coordinate of Point 1: ";
+s = System.console().readLine();
+point2.y = Double.parseDouble(s);
+
+
+
 class Point {
 	double x;
 	double y;
