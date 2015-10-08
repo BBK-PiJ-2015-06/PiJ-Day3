a4b46ad2fc7cab6ca640255b62927c4429a8b5d3
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-10-08 11:03:16 +0100

    Program now considers whether the 3 points are equidistant from eachother.

--- E12_distanceP2P.groovy
+++ E12_distanceP2P.groovy
@@ -31,15 +31,22 @@ println " ";
 double distanceP1P2 = Math.abs(point2.x - point1.x) + Math.abs(point2.y - point1.y);
 double distanceP1P3 = Math.abs(point3.x - point1.x) + Math.abs(point3.y - point1.y);
 double distanceP2P3 = Math.abs(point3.x - point2.x) + Math.abs(point3.y - point2.y);
-if (distanceP1P2 <= distanceP1P3 && distanceP1P2 <= distanceP2P3) {
-	println "Points 1 and 2 are closest.";
-} 
-if (distanceP1P3 <= distanceP1P2 && distanceP1P3 <= distanceP2P3) {
-	println "Points 1 and 3 are closest.";
+if (distanceP1P2 == distanceP1P3 && distanceP1P3 == distanceP2P3) {
+	println "All three points are equidistant.";
+} else {
+	if (distanceP1P2 <= distanceP1P3 && distanceP1P2 <= distanceP2P3) {
+		println "Points 1 and 2 are closest.";
+	} 
+	if (distanceP1P3 <= distanceP1P2 && distanceP1P3 <= distanceP2P3) {
+		println "Points 1 and 3 are closest.";
+	}
+	if (distanceP2P3 <= distanceP1P2 && distanceP2P3 <= distanceP1P3) {
+		println "Points 2 and 3 are closest.";
+	}
 }
-if (distanceP2P3 <= distanceP1P2 && distanceP2P3 <= distanceP1P3) {
-	println "Points 2 and 3 are closest.";
-}
+println "distanceP1P2: " + distanceP1P2
+println "distanceP1P3: " + distanceP1P3
+println "distanceP2P3: " + distanceP2P3
 class Point {
 	double x;
 	double y;
