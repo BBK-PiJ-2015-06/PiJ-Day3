336b69d68fdd16be40b8e8c9bdf7244c736cf386
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-10-08 18:46:28 +0100

    Program works without spaces between operands and numbers.

--- E02_cmdCalculator.groovy
+++ E02_cmdCalculator.groovy
@@ -1,14 +1,24 @@
 println "Please enter text representing a mathematical operation: ";
 String s = System.console().readLine();
 int l = s.length();
-for (int count = 0; count < l; count++) {
-	char c = charAt(count);
-	if (c == '+' || c == '-' || c == '*' || c == '/' || c == ' ') {
-		if (c != ' ') {
-			char operand = c;
-		}
+int gap = s.length();
+String numbers = s.substring(0,1);
+char operand = '0';
+for (int count = 1; count < l; count++) {
+	char c = s.charAt(count);
+	if (c == '+' || c == '-' || c == '*' || c == '/') {
+		operand = c;
+		gap = count
+	} else if (c == ' ') {
+		break;
+	} else {
+		numbers = numbers + c;
 	}
 }
+String firstnum = numbers.substring(0,gap);
+String secondnum = numbers.substring(gap);
+double num1 = Double.parseDouble(firstnum);
+double num2 = Double.parseDouble(secondnum);
 double output = 0;
 switch (operand) {
 case '+':
@@ -26,3 +36,4 @@ case '/':
 default:
 	break;
 }
+println num1 + " " + operand + " " + num2 + " = " + output;
