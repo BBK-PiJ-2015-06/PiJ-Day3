7b750aeb5235546e5f158f22f6decd59ae98f530
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-10-08 12:09:27 +0100

    Solution to exercise 13 completed.

--- E13_rectangle.groovy
+++ E13_rectangle.groovy
@@ -9,17 +9,20 @@ Point point2 = new Point();
 print "Please input x coordinate of Point 2: ";
 s = System.console().readLine();
 point2.x = Double.parseDouble(s);
-print "Please input y coordinate of Point 1: ";
+print "Please input y coordinate of Point 2: ";
 s = System.console().readLine();
 point2.y = Double.parseDouble(s);
-
-
-
+Rectangle myRectangle = new Rectangle();
+myRectangle.upLeft = point1;
+myRectangle.downRight = point2;
+double area = Math.abs(myRectangle.downRight.x - myRectangle.upLeft.x)*Math.abs(myRectangle.downRight.y - myRectangle.upLeft.y);
+double perimeter = 2*(Math.abs(myRectangle.downRight.x - myRectangle.upLeft.x) + Math.abs(myRectangle.downRight.y - myRectangle.upLeft.y));
+println "Area: " + area;
+println "Perimeter: " + perimeter;
 class Point {
 	double x;
 	double y;
 }
-
 class Rectangle {
 	Point upLeft;
 	Point downRight;
