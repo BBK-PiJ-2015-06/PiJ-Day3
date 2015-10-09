f56726043bc3ffe65b67c2dc3dbde0f5d9b8c0d9
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-10-09 08:47:27 +0100

    First commit. Just asks for user's input.

---
+++ E06_countingPairsLetters.groovy
@@ -0,0 +1,4 @@
+println "Please type below a short line of text:";
+String short = System.console().readLine();
+println "Please type below a longer line of text within which the first line needs to be found:";
+String long = System.console().readLine();
